236b741d066845fd5edd251bcae4658e9f147356
ruiming0114 <ruiming0114@buaa.edu.cn>
2020-12-15 18:38:51 +0800

    修改高级检索的传参

--- src/components/patentSearchResult.vue
+++ src/components/patentSearchResult.vue
@@ -176,7 +176,7 @@ export default {
       ],
     };
   },
-  props: ["word"],
+  props: ["word", "institution", "author", "startDate", "endDate"],
   methods: {
     changePage() {
       console.log(this.currentPage);
