4d6ff6884df8980b15ca9bb7f4d7563413db5db8
Tuanz_lu <tuanz_lu@Tuanz-ludeMacBook-Pro.local>
2020-12-17 21:49:16 +0800

    update

--- src/api/path.js
+++ src/api/path.js
@@ -19,7 +19,7 @@ let path = {
     userVerify: Ip.productIP + "user/verify",
     scholarVerify: Ip.productIP + "user/toBeScholar",
     getInfo: Ip.productIP + "user/getInfo",
-    
+
     //检索
     searchProject: Ip.productIP + "scholarship/advancedSearchProject",
     searchProjectSortByDate: Ip.productIP + "scholarship/advancedSearchProjectSortByDate",
@@ -33,16 +33,19 @@ let path = {
 
     //申诉列表
     getAppeal: Ip.productIP + "message/appeal/all/",
-    updateAppeal: Ip.productIP + "message/appeal/one/", 
-    
+    updateAppeal: Ip.productIP + "message/appeal/one/",
+
     //学者
     getSameNameScholar: Ip.productIP + "scholar/sameName",
     subscribe: Ip.productIP + "scholar/subscribe",
     undoSubscribe: Ip.productIP + "scholar/subscribe",
-    getScholarInfo: Ip.productIP + "scholar",
-    editScholarInfo: Ip.productIP + "scholar",
-    claimDataPortal: Ip.productIP + "scholar/scholar_dataScholar",
-    undoClaimDataPortal: Ip.productIP + "scholar/scholar_dataScholar",
+    getScholarInfo: Ip.productIP + "scholar/info",
+    editScholarInfo: Ip.productIP + "scholar/infoUpdate",
+    claimDataPortal: Ip.productIP + "scholar/Scholar_DataScholar",
+    undoClaimDataPortal: Ip.productIP + "scholar/Scholar_DataScholar",
+    getDataPortal: Ip.productIP + "scholar/Scholar_DataScholar",
+    addWorkExp: Ip.productIP + "scholar/workExperience",
+    deleteWorkExp: Ip.productIP + "scholar/workExperience",
   },
 };
 export default path;
--- src/views/personalWorkplace/ClaimScholar.vue
+++ src/views/personalWorkplace/ClaimScholar.vue
@@ -2,6 +2,8 @@
   <p>如果你看到了这条信息，说明程序员摸了</p>
 </template>
 <script>
-export default {};
+export default {
+  name: "ClaimScholar",
+};
 </script>
 <style scoped></style>>
